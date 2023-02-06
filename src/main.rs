a581223588aebc2b47064374762738feea5a16e1
casey-SK <56776006+casey-SK@users.noreply.github.com>
2023-02-05 20:03:35 -0600

    better start

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,8 @@
 
-
-use rand::Rng; 
-
-
 fn main() {
 
-    // "hello, world!\n" in a bunch of chunks of between 1 and 3 bytes, random. 
-    let input_line = "Hello, World!\n".as_bytes();
-    println!("{:?}", input_line);
-
-    let minus_1 = [b'\0'; 63];
-    let minus_2 = [b'\0'; 62];
-    let minus_3 = [b'\0'; 61];
-
-
-    // Manually create random data
-    let in_1 = [&input_line[..1], minus_1.as_slice()].concat();
-    let in_2 = [&input_line[1..3], minus_2.as_slice()].concat();
-    let in_3 = [&input_line[3..4], minus_1.as_slice()].concat();
-    let in_4 = [&input_line[4..7], minus_3.as_slice()].concat();
-    let in_5 = [&input_line[7..9], minus_2.as_slice()].concat();
-    let in_6 = [&input_line[9..11], minus_2.as_slice()].concat();
-    let in_7 = [&input_line[11..12], minus_1.as_slice()].concat();
-    let in_8 = [&input_line[12..14], minus_2.as_slice()].concat();
-
-    let input_stream = [
-        in_1.as_slice(), 
-        in_2.as_slice(), 
-        in_3.as_slice(), 
-        in_4.as_slice(), 
-        in_5.as_slice(), 
-        in_6.as_slice(), 
-        in_7.as_slice(), 
-        in_8.as_slice(), 
-        ];
+    let input_stream = create_input_data();
     
-    for i in input_stream {
-        // we can confirm that our amatuer built random input generator works
-        // we expect to see a bunch of slices of len 64 ONLY
-        println!("{:?}", i.len());
-    }
-
-
-    //println!("{:?}", &input_stream);
 }
 
 
